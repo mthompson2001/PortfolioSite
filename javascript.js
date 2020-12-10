6675a20e18f28341ffdec3b4d961004f13d1de34
Your Name <you@example.com>
2020-12-10 12:55:18 +0000

    I have now finalized the code for my image carousel, by making the images loop round to avoid white space, I've hidden the overflow to make the overall look much cleaner and I have imported an external stylesheet from fontawesome to add in navigational arrows for the carousel. My next aim is to begin finalizing the design of the home page, and also implementing additional pages which will accessed via hyperlinks. These pages will consist of my different collections of images, allwoing the viewers to individualy navigate singualar images.

--- javascript.js
+++ javascript.js
@@ -25,3 +25,19 @@ prevBtn.addEventListener('click',()=> {
 	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 	
 });
+
+carouselSlide.addEventListener('transitionend', () => {
+	console.log(carouselImages[counter]);
+	if (carouselImages[counter].id === 'lastClone') {
+		carouselSlide.style.transition = "none";
+		counter = carouselImages.length - 2;
+		carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+		
+	}
+	if (carouselImages[counter].id === 'firstClone') {
+		carouselSlide.style.transition = "none";
+		counter = carouselImages.length - counter;
+		carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+		
+	}
+});
